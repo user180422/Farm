d941032e3004c78c19924a2bc428c7b9aafe213f
user180422 <inb498@gmail.com>
2023-11-29 18:35:05 +0530

    adding get quotes

--- Backend/public/js/getQuote.js
+++ Backend/public/js/getQuote.js
@@ -44,3 +44,83 @@ function logout() {
     document.cookie = "farm= ";
     window.location.href = 'index.html'
 }
+
+function submitHandler() {
+    // Add your form ID or class if it's specific to one form
+    var form = document.querySelector('quoteForm');
+
+    console.log("working");
+
+    form.addEventListener('submit', function (event) {
+        // Prevent the form from submitting by default
+        event.preventDefault();
+
+        // Reset previous error messages
+        resetErrors();
+
+        // Validate each field
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var countryCode = document.getElementById('countryCode').value;
+        var phone = document.getElementById('phone').value;
+
+        validateName(name);
+        validateEmail(email);
+        validateCountryCode(countryCode);
+        validatePhone(phone);
+
+        // If all validations pass, you can submit the form
+        if (validateName(name) && validateEmail(email) && validateCountryCode(countryCode) && validatePhone(phone)) {
+            // Your form submission logic goes here
+            console.log('Form submitted successfully!');
+        }
+    });
+};
+
+function validateName(name) {
+    var nameRegex = /^[a-zA-Z\s]+$/;
+    var nameError = document.getElementById('nameError');
+
+    if (!nameRegex.test(name)) {
+        nameError.textContent = 'Invalid name. Only alphabets and spaces are allowed.';
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var emailError = document.getElementById('emailError');
+
+    if (!emailRegex.test(email)) {
+        emailError.textContent = 'Invalid email address.';
+        return false;
+    }
+    return true;
+}
+
+function validateCountryCode(countryCode) {
+    var countryCodeError = document.getElementById('countryCodeError');
+
+    // You can add your specific validation logic for the country code if needed
+
+    return true;
+}
+
+function validatePhone(phone) {
+    var phoneRegex = /^\d{10}$/; // Change this regex based on your phone number format
+    var phoneError = document.getElementById('phoneNoError');
+
+    if (!phoneRegex.test(phone)) {
+        phoneError.textContent = 'Invalid phone number. It should be 10 digits.';
+        return false;
+    }
+    return true;
+}
+
+function resetErrors() {
+    var errorContainers = document.querySelectorAll('.error-message');
+    errorContainers.forEach(function (container) {
+        container.textContent = '';
+    });
+}
--- Backend/public/js/register.js
+++ Backend/public/js/register.js
@@ -44,3 +44,142 @@ function logout() {
     document.cookie = "farm= ";
     window.location.href = 'index.html'
 }
+
+function failedClosePopup(popupId) {
+    var popup = document.getElementById("failedPopup");
+    popup.style.display = 'none';
+}
+function showFailedPopup(popupId) {
+    var popup = document.getElementById("failedPopup");
+    popup.style.display = 'block';
+}
+
+function validateAndSubmit(event) {
+    const failedMsg = document.querySelector(".failedMsg");
+
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirmPassword').value;
+
+    console.log("confirm password", confirmPassword);
+
+    // Reset previous error messages
+    resetErrors();
+
+    // Validate username
+    if (!username) {
+        document.getElementById('usernameError').innerText = 'Username is required.';
+        return;
+    } else {
+        var usernameRegex = /^[a-zA-Z0-9_]+$/;
+        if (!usernameRegex.test(username)) {
+            document.getElementById('usernameError').innerText = 'Invalid username. Only letters, numbers, and underscores are allowed.';
+            return;
+        }
+    }
+
+    // Validate email
+    if (!email) {
+        document.getElementById('emailError').innerText = 'Email is required.';
+        return;
+    } else {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            document.getElementById('emailError').innerText = 'Invalid email format.';
+            return;
+        }
+    }
+
+    // Validate phone
+    if (!phone) {
+        document.getElementById('phoneError').innerText = 'Phone number is required.';
+        return;
+    } else {
+        var phoneRegex = /^\d{10}$/; // Change this regex based on your phone number format
+        if (!phoneRegex.test(phone)) {
+            document.getElementById('phoneError').innerText = 'Invalid phone number. It should be 10 digits.';
+            return;
+        }
+    }
+
+    // Validate password
+    if (!password) {
+        document.getElementById('passwordError').innerText = 'Password is required.';
+        return;
+    } else if (!/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(password)) {
+        document.getElementById('passwordError').innerText = 'Password must contain at least 8 characters with at least one uppercase letter, one number, and one special character';
+        return
+    }
+
+    // Validate confirm password
+    if (!confirmPassword) {
+        document.getElementById('confirmPasswordError').innerText = 'Please confirm your password.';
+        return;
+    } else {
+        var confirmPasswordError = document.getElementById('confirmPasswordError');
+        if (password !== confirmPassword) {
+            confirmPasswordError.innerText = 'Passwords do not match.';
+            return;
+        }
+    }
+
+    if (!username || !email || !phone || !password || !confirmPassword) {
+        return;
+    }
+
+    const data = {
+        username: username,
+        email: email,
+        phone: phone,
+        password: password,
+        confirmPassword: confirmPassword
+    };
+
+    fetch('http://localhost:4000/api/createUser', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Registration successful, you can redirect or handle it accordingly
+                document.getElementById('username').value = ""
+                document.getElementById('email').value = ""
+                document.getElementById('phone').value = ""
+                document.getElementById('password').value = ""
+                document.getElementById('confirmPassword').value = ""
+                failedMsg.innerHTML = data.success
+                setTimeout(() => {
+                    failedClosePopup();
+                }, 5000);
+                showFailedPopup();
+            } else {
+                // Registration failed, display error message
+                failedMsg.innerHTML = data.error;
+                setTimeout(() => {
+                    failedClosePopup();
+                }, 5000);
+                showFailedPopup();
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            failedMsg.innerHTML = 'An error occurred. Please try again later.';
+            showFailedPopup();
+        });
+}
+
+function resetErrors() {
+    var errorContainers = document.querySelectorAll('.error-message');
+    console.log("reset");
+    errorContainers.forEach(function (container) {
+        container.textContent = '';
+    });
+}
